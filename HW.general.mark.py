9a218d9107c69edc1a5f8dc2843e3a862f345acb
Mariana Terintii <117921869+marianaterintii@users.noreply.github.com>
2023-02-28 12:00:46 +0200

    Add files via upload

---
+++ HW.general.mark.py
@@ -0,0 +1,35 @@
+
+
+# create a function (generalMark) which recieves 3 numbers from a dictionary
+def generalMark(average):
+    average_mark = (marks["sem_1"] + marks["sem_2"]+ marks["exam"])/3
+    marks["average"] = average_mark #adds to the dictionary a new key and value
+    return average
+
+marks = {
+        "sem_1": 9.0,
+        "sem_2": 8.0,
+        "exam" : 9.0,    
+    }
+
+print(generalMark(marks))
+
+'''
+           marks .......{ }
+              |         /
+              v        /
+generalMark(general)  /
+              |      /
+              v     /
+            general/
+              |
+              v
+         average_mark ---> read
+              |
+              v   
+              (marks["sem_1"] + marks["sem_2"]+ marks["exam"])/3  
+              |
+              V
+         marks["average"] = average_mark
+
+'''
